f0ddcc5a9809e565939eab405cd8454a0ec32612
pablojg9 <pablojuniorgn2@gmail.com>
2022-01-20 20:55:25 -0300

    Projeto Rest Spring Boot

---
+++ src/test/java/br/com/pablo/rest/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pablo.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
