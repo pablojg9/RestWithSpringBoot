773a21911611d5a803c8545e30f18a79df497289
pablojg9 <pablojuniorgn2@gmail.com>
2022-01-20 22:50:50 -0300

    Adicionado todas operações

--- src/main/java/br/com/pablo/rest/controllers/MathController.java
+++ src/main/java/br/com/pablo/rest/controllers/MathController.java
@@ -1,7 +1,10 @@
 package br.com.pablo.rest.controllers;
 
 import br.com.pablo.rest.exceptions.UnsupportedMathOperationException;
+import br.com.pablo.rest.services.MathService;
 import br.com.pablo.rest.utils.MessageUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,33 +13,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathController {
 
-    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
+    @Autowired
+    private MathService mathService;
 
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+
+        if (!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException(MessageUtil.MESSAGE_EXCEPTION_ERROR_BAD_REQUEST);
         }
+        return mathService.converterTwoDouble(numberOne) + mathService.converterTwoDouble(numberTwo);
 
-        Double sum = converterTwoDouble(numberOne) + converterTwoDouble(numberTwo);
-
-        return sum;
     }
 
-    private Double converterTwoDouble(String strNumber) {
+    @RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
 
-        if (strNumber == null) return 0D;
-        String number = strNumber.replaceAll(",", ".");
-        if (isNumeric(number)) return Double.parseDouble(number);
-
-        return 0D;
+        if (!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException(MessageUtil.MESSAGE_EXCEPTION_ERROR_BAD_REQUEST);
+        }
+        return mathService.converterTwoDouble(numberOne) - mathService.converterTwoDouble(numberTwo);
     }
 
-    private boolean isNumeric(String strNumber) {
 
-        if (strNumber == null) return false;
-        String number = strNumber.replaceAll(",", ".");
+    @RequestMapping(value = "/multi/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double multi(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
 
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+        if (!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException(MessageUtil.MESSAGE_EXCEPTION_ERROR_BAD_REQUEST);
+        }
+        return mathService.converterTwoDouble(numberOne) * mathService.converterTwoDouble(numberTwo);
     }
 
+    @RequestMapping(value = "/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+
+        if (!mathService.isNumeric(numberOne) || !mathService.isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException(MessageUtil.MESSAGE_EXCEPTION_ERROR_BAD_REQUEST);
+        }
+        return mathService.converterTwoDouble(numberOne) / mathService.converterTwoDouble(numberTwo);
+    }
 }
+
---
+++ src/main/java/br/com/pablo/rest/services/MathService.java
@@ -0,0 +1,28 @@
+package br.com.pablo.rest.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MathService {
+
+    public Double converterTwoDouble(String strNumber) {
+
+        if (strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number)) return Double.parseDouble(number);
+
+        return 0D;
+    }
+
+    public boolean isNumeric(String strNumber) {
+
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+
+
+
+
+}
